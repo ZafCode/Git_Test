ec4f7a12ad9d63e08b209d5784720bbb98f84871
ZafCode <zrealtytx@gmail.com>
2020-08-23 22:52:48 -0500

    Cok Seker

--- src/main/java/Cay/Seker.java
+++ src/main/java/Cay/Seker.java
@@ -1,8 +1,9 @@
 package Cay;
 
 public class Seker {
+
     public static void main(String[] args) {
-        System.out.println("cift sekers");
-        System.out.println("cift sekers ");
+        System.out.println("cook sekers");
+
     }
 }
