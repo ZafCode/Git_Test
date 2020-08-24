ba8a88e6ce0ea03873155ab94a4df424e715584d
ZafCode <zrealtytx@gmail.com>
2020-08-23 22:53:58 -0500

    cift sekers

--- src/main/java/Cay/Seker.java
+++ src/main/java/Cay/Seker.java
@@ -2,7 +2,7 @@ package Cay;
 
 public class Seker {
     public static void main(String[] args) {
-        System.out.println("tek seker");
-        System.out.println("tek seker");
+        System.out.println("cift Sekers");
+
     }
 }
